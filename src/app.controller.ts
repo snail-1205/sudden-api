07db00aafdac7417ca6c6c764ae190c337591840
snail-1205 <junha120586@gmail.com>
2022-12-23 01:37:47 +0900

    asdas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { AppService, DefaultRes } from './app.service';
 import { Request } from 'express';
+import "reflect-metadata"
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+    
+  }
 
   @Get()
   getHello(): string {
